596ddef5e2edbe96a6c23d0d2b46e7f55b31a46f
fatroo <rm552184@fiap.com.br>
2024-03-20 11:28:58 -0300

    att

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import {AsyncStorage} from 'react-native-async-storage/async-storage';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, A } from "react-native";
 
+ 
+ 
 export default function App() {
+ 
+  const[nomeProduto, setNomeProduto] = useState('')
+  const[precoProduto, setPrecoProduto] = useState('')
+  async function salvar(){
+    await AsyncStorage.setItem('Produtos', nomeProduto+precoProduto)
+    alert('Produto Cadastrado')
+ }
+ async function buscarDasdos(){
+  const p = await AsyncStorage.getItem("Produtos")
+  console.log(p)
+ }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Cadastro</Text>
+      <TextInput placeholder="Digite o nome do produto" style={styles.input} value={nomeProduto} onChangeText={(value)=> setNomeProduto(value)} />
+      <TextInput placeholder="Digite o preço do produto" style={styles.input} value={precoProduto} onChangeText={(value)=> setPrecoProduto(value)}/>
+ 
+      <TouchableOpacity style={styles.btn} onPress={salvar}>
+        <Text style={{color: '#fff', fontSize: 12}}>CADASTRAR</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.btn} onPress={buscarDasdos}>
+        <Text style={{color: '#fff', fontSize: 12}}>Buscar Dados</Text>
+      </TouchableOpacity>
     </View>
   );
 }
-
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10
+  },
+  input: {
+    borderWidth: 1,
+    width: 300,
+    height: 30,
+    borderRadius: 5,
+    paddingLeft: 5
+  },
+  btn: {
+    borderWidth: 1,
+    width: 200,
+    height: 30,
+    borderRadius: 50,
     justifyContent: 'center',
+    backgroundColor: '#000',
+    alignItems:'center'
   },
 });
+ 
